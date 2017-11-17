029b0093a2bbb612e8788fdcf7448882d9b9ed77
Roberta Bustos <yourowndisaster0@gmail.com>
2017-11-17 19:51:47 +0800

    Add v2

---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+var config = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader'
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: function () {
+                                    return [
+                                        require('precss'),
+                                        require('autoprefixer')
+                                    ];
+                                }
+                            }
+                        },
+                        {
+                            loader: 'sass-loader' // compiles Sass to CSS
+                        }
+                    ]
+                })
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.$': 'jquery',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default'],
+            'Tether': 'tether'
+        }),
+        new ExtractTextPlugin('style.css'),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+            inject: false,
+            minify: {
+                collapseWhitespace: true,
+                preserveLineBreaks: true
+            }
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+        hot: false,
+        inline: false
+    }
+};
+
+if (process.env.NODE_ENV === 'production') {
+    config.devtool = 'source-map';
+    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true,
+        compress: {
+            warnings: true
+        }
+    }));
+    config.plugins.push(new webpack.LoaderOptionsPlugin({
+        minimize: true
+    }));
+}
+
+module.exports = config;
